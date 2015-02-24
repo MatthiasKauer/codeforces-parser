649d6c0aad4145620897fd9d3e275417dc669e9b
matthias <software@matthiaskauer.com>
2015-02-24 21:39:36 +0800

    added to python template

--- myprog.py
+++ myprog.py
@@ -1,4 +1,8 @@
-import math,string,itertools,fractions,heapq,collections,re,array,bisect
+from __future__ import print_function
+import sys,math,string,itertools,fractions,heapq,collections,re,array,bisect
+
+def debug(s):
+    print(s, file=sys.stderr)
 
 if __name__ == '__main__':
-    n=int(raw_input())
+    a, b, s = [int(r) for r in raw_input().split()]

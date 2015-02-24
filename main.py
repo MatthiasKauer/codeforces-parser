a3484aeaa648221014c2ebd8833c9bfd342b29d4
matthias <software@matthiaskauer.com>
2015-02-24 22:08:35 +0800

    adjusted debug print in pyton template

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import sys,math,string,itertools,fractions,heapq,collections,re,array,bisect
 
-def debug(s):
-    print(s, file=sys.stderr)
+def debug(*args):
+    print(args, file=sys.stderr)
 
 if __name__ == '__main__':
     a, b, s = [int(r) for r in raw_input().split()]
